6630cfada6a1573dc33fcd1c2b48d86e62016e90
navdeep0905 <122873138+navdeep0905@users.noreply.github.com>
2025-03-17 10:46:05 +0000

    container config done

---
+++ 09_basics/datatypes-03.js
@@ -0,0 +1,10 @@
+let score="false"
+console.log(typeof score);
+
+let valueInnumber=Number(score)
+console.log(typeof valueInumber);
+console.log(valueInnumber);  
+
+let isloggedIn=1
+let booleanIsLoggedIn=boolean(IsLoggedIn)
+console.logboolean(IsLoggedIn);
---
+++ 09_basics/datatypes.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log(5+5)
+
+console.log("navdeep")
+
+let name="navdeep"
+let age=89
+let isloggedIn=false
+console.log(typeof"navdeep");
+console.log(typeof age)
+console.log(typeof null);
+console.log(typeof undefined);
